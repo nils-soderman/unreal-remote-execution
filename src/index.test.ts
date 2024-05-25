d376d92369316dd7b7949d56bb20630279858e1d
Nils Söderman <rymdnisse@live.com>
2024-05-25 17:05:52 +0200

    Added a basic test using jest

---
+++ src/index.test.ts
@@ -0,0 +1,52 @@
+/**
+ * This tests require a running instance of Unreal Engine with the RemoteExecution plugin enabled.
+ */
+
+import { RemoteExecution, ECommandOutputType, RemoteExecutionConfig } from './index';
+
+// These settings must match the settings in the Unreal Engine plugin, or the tests will fail.
+const LOCALHOST_IP = '127.0.0.1';
+const MULTICAST_ENDPOINT_IP = '239.0.0.1';
+const MULTICAST_ENDPOINT_PORT = 6766;
+const COMMAND_ENDPOINT_PORT = 6776;
+
+
+describe('RemoteExecution', () => {
+  let remoteExecution: RemoteExecution;
+
+  beforeEach(() => {
+    const config = new RemoteExecutionConfig(
+      0,
+      [MULTICAST_ENDPOINT_IP, MULTICAST_ENDPOINT_PORT],
+      LOCALHOST_IP,
+      [LOCALHOST_IP, COMMAND_ENDPOINT_PORT]
+    );
+
+    remoteExecution = new RemoteExecution(config);
+  });
+
+  afterAll(() => {
+    remoteExecution.stop();
+  });
+
+  test('Hello World', async () => {
+    await remoteExecution.start();
+
+    const node = await remoteExecution.getFirstRemoteNode(1000, 2000);
+
+    await remoteExecution.openCommandConnection(node);
+
+    const response = await remoteExecution.runCommand('print("Hello World")');
+
+    expect(response.success).toBe(true);
+    expect(response.result).toBe("None");
+
+    expect(response.output.length).toBe(1);
+  
+    for (const output of response.output) {
+      expect(output.type).toBe(ECommandOutputType.INFO);
+      expect(output.output).toBe('Hello World\r\n');
+    }
+  });
+
+});
